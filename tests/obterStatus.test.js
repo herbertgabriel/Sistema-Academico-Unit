510c27e1046981e37bd79c3b6ee5e256808375fc
Everlyn Marques <163144541+evemqs@users.noreply.github.com>
2024-11-12 22:46:10 -0300

    Update obterStatus.test.js

--- tests/obterStatus.test.js
+++ tests/obterStatus.test.js
@@ -1,4 +1,4 @@
-const { obterStatus } = require('../src/exibirStatus');
+const { obterStatus } = require('../src/obterStatus');
 
 test('Média inferior a 4.0', () => {
   expect(obterStatus(3.5)).toBe('Reprovado por média');
